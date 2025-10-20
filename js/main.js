f3ad188d0894ed81aef59ac5ab6b3c73d282911f
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-10-20 07:47:56 +0200

    Add initial files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+const kittenone=`
+<li class="card">
+            <article>
+              <img
+                class="card_img"
+                src="https://dev.adalab.es/gato-siames.webp"
+                alt="siames-cat"
+              />
+              <h3 class="card_title">Anastacio</h3>
+              <h4 class="card_race">Siamés</h4>
+              <p class="card_description">
+                Porte elegante, su patrón de color tan característico y sus ojos
+                de un azul intenso, pero su historia se remonta a Asía al menos
+                hace 500 años, donde tuvo su origen muy posiblemente.
+              </p>
+            </article>
+          </li>`;
+document.querySelector(".js-list").innerHTML = kittenone
