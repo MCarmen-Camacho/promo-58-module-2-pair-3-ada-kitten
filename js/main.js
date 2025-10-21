09847d0dbcdae770ab67b7c767ba6e643db5b81a
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-10-21 10:04:56 +0200

    Keep working on lesson 2.2 exercise

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
-const kittenone=`
+
+const kittenOne = `
 <li class="card">
             <article>
               <img
@@ -16,4 +17,41 @@ const kittenone=`
               </p>
             </article>
           </li>`;
-document.querySelector(".js-list").innerHTML = kittenone
+
+const kittenTwo = `
+<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/sphynx-gato.webp"
+              alt="sphynx-cat"
+            />
+            <h3 class="card_title">Fiona</h3>
+            <h4 class="card_race">Sphynx</h4>
+            <p class="card_description">
+              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+              hasta con pinta de alienígena han llegado a definir a esta raza
+              gatuna que se caracteriza por la «ausencia» de pelo.
+            </p>
+          </li>`;
+
+const kittenThree = `
+<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/maine-coon-cat.webp"
+              alt="maine-coon-cat"
+            />
+            <h3 class="card_title">Cielo</h3>
+            <h4 class="card_race">Maine Coon</h4>
+            <p class="card_description">
+              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+              bella mirada se ha convertido en una de sus señas de identidad.
+              Sus ojos son grandes y las orejas resultan largas y en punta.
+            </p>
+          </li>`;
+
+const jsBtnAdd = ``;
+
+document.querySelector(".js-list").innerHTML = kittenOne;
+document.querySelector(".js-list").innerHTML += kittenTwo;
+document.querySelector(".js-list").innerHTML += kittenThree;
