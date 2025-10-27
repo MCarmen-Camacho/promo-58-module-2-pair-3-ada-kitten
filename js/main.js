9c57cf60c3637b594573c200941aeb9011c1e7a4
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-10-27 16:46:47 +0100

    Exercise 2.7

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 "use strict";
 
-const kittenDesc1 = `Porte elegante, su patrón de color tan característico y sus ojos
+// ========================
+// MOSTRAR Y BUSCAR GATITOS
+// ========================
+
+// QUERY-SELECTOR
+
+const kittensUl = document.querySelector(".js-list");
+const searchButton = document.querySelector(".js_button-search");
+const descrSearchText = document.querySelector(".js_in_search_desc");
+
+// DATOS
+
+const kittenDesc1 = {
+  image: "https://dev.adalab.es/gato-siames.webp",
+  name: "Anastacio",
+  desc: `Porte elegante, su patrón de color tan característico y sus ojos
                 de un azul intenso, pero su historia se remonta a Asía al menos
-                hace 500 años, donde tuvo su origen muy posiblemente.`;
+                hace 500 años, donde tuvo su origen muy posiblemente.`,
+  race: "Siamés",
+};
+
+// Plantilla con el li de cada gato donde la rellenamos con los datos del gato 1 que proviene de un objeto.
 const kittenOne = `
 <li class="card">
             <article>
               <img
                 class="card_img"
-                src="https://dev.adalab.es/gato-siames.webp"
+                src=${kittenDesc1.image}
                 alt="siames-cat"
               />
-              <h3 class="card_title">Anastacio</h3>
-              <h4 class="card_race">Siamés</h4>
-              <p class="card_description">${kittenDesc1}
+              <h3 class="card_title">${kittenDesc1.name}</h3>
+              <h4 class="card_race">${kittenDesc1.race}</h4>
+              <p class="card_description">${kittenDesc1.desc}
               </p>
             </article>
           </li>`;
@@ -51,26 +70,11 @@ const kittenThree = `
             </p>
           </li>`;
 
-document.querySelector(".js-list").innerHTML = kittenOne;
-document.querySelector(".js-list").innerHTML += kittenTwo;
-document.querySelector(".js-list").innerHTML += kittenThree;
+// FUNCIONES
 
-const btnAdd = document.querySelector(".js-btn-add");
-const sectionNewForm = document.querySelector(".js-new-form");
+// EVENTOS
 
-btnAdd.addEventListener(`click`, (ev) => {
-  sectionNewForm.classList.toggle("collapsed");
-});
-
-const btnCancel = document.querySelector(".js_btn_cancel");
-
-btnCancel.addEventListener("click", (ev) => {
-  sectionNewForm.classList.toggle("collapsed");
-});
-
-const searchButton = document.querySelector(".js_button-search");
-const descrSearchText = document.querySelector(".js_in_search_desc");
-
+// Evento que filtra y muestra los gatitos que contengan el texto introducido en el campo de búsqueda
 searchButton.addEventListener("click", (ev) => {
   ev.preventDefault();
   const descrValue = descrSearchText.value;
@@ -85,3 +89,47 @@ searchButton.addEventListener("click", (ev) => {
     document.querySelector(".js-list").innerHTML += kittenThree;
   }
 });
+
+// CODIGO AL CARGAR LA PAG.
+
+// Mostrar los gatitos
+// Pone los <li> dentro del <ul>
+
+kittensUl.innerHTML = kittenOne;
+kittensUl.innerHTML += kittenTwo;
+kittensUl.innerHTML += kittenThree;
+
+// ========================
+// FORMULARIO NUEVO GATITO
+// ========================
+
+// QUERY-SELECTOR
+
+const btnAdd = document.querySelector(".js-btn-add");
+const sectionNewForm = document.querySelector(".js-new-form");
+const btnCancel = document.querySelector(".js_btn_cancel");
+
+// DATOS
+
+// FUNCIONES
+
+function showNewCatForm() {
+  newForm.classList.remove("collapsed");
+}
+
+function hideNewCatForm() {
+  newForm.classList.add("collapsed");
+}
+
+// EVENTOS
+
+btnAdd.addEventListener(`click`, (ev) => {
+  // sectionNewForm.classList.remove("collapsed");
+  showNewCatForm();
+});
+
+btnCancel.addEventListener("click", (ev) => {
+  sectionNewForm.classList.toggle("collapsed");
+});
+
+// CODIGO AL CARGAR LA PAG.
